55950036465254d66dad4e8930ecc926f899d282
tpkbuleleng <admin@ipekbbuleleng.com>
2026-03-04 23:14:53 +0800

    Update data-kader.js

--- data-kader.js
+++ data-kader.js
@@ -1,13 +1,13 @@
-// FORMAT: "ID KADER": "NIK / PASSWORD" (16 Digit)
+// FORMAT: "ID KADER": { nik: "16 DIGIT", nama: "NAMA LENGKAP KADER" }
 const dataAkunKader = {
     // TIM 01 (Desa 2001, Kecamatan 81)
-    "812001-01001": "5108010101010001",
-    "812001-01002": "5108010101010002",
-    "812001-01003": "5108010101010003",
+    "812001-01001": { nik: "5108010101010001", nama: "Ni Wayan Sari" },
+    "812001-01002": { nik: "5108010101010002", nama: "Kadek Ayu Lestari" },
+    "812001-01003": { nik: "5108010101010003", nama: "Komang Budiarti" },
     
     // TIM 02 (Desa 2001, Kecamatan 81)
-    "812001-02004": "5108010101010004",
+    "812001-02004": { nik: "5108010101010004", nama: "Ketut Suwarningsih" },
 
     // TIM 01 (Desa 2002, Kecamatan 81)
-    "812002-01005": "5108020101010005"
+    "812002-01005": { nik: "5108020101010005", nama: "Putu Indah Maharani" }
 };
