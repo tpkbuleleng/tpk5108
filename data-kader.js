6eaa1189407771556720c539032e13dd8d358429
tpkbuleleng <admin@ipekbbuleleng.com>
2026-03-04 14:44:28 +0800

    Create data-kader.js

---
+++ data-kader.js
@@ -0,0 +1,16 @@
+// Daftar Akun Kader (Format: "ID KADER": "PASSWORD / NIK")
+// Format ID: [Kecamatan 2 digit][Desa 4 digit]-[Urut Tim 2 digit][Urut Kader 3 digit]
+const dataAkunKader = {
+    // TIM 01 (Desa 2001, Kecamatan 81)
+    "812001-01001": "5108010101010001",
+    "812001-01002": "5108010101010002",
+    "812001-01003": "5108010101010003",
+    
+    // TIM 02 (Desa 2001, Kecamatan 81)
+    "812001-02004": "5108010101010004",
+
+    // TIM 01 (Desa 2002, Kecamatan 81)
+    "812002-01005": "5108020101010005"
+    
+    // Lanjutkan menaruh data kader lainnya ke bawah...
+};
