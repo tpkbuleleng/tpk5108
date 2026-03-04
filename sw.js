061faa5f83f81abf4bab028b6af42fffa5450e88
tpkbuleleng <admin@ipekbbuleleng.com>
2026-03-04 15:20:31 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,40 +1,26 @@
-// UBAH NAMA VERSI INI MENJADI v3
-const CACHE_NAME = 'tpk-buleleng-v2';
+const CACHE_NAME = 'tpk-buleleng-v9';
 
 const urlsToCache = [
   './',
   './index.html',
   './TPK BLL26.png',
-  './manifest.json'
+  './manifest.json',
+  './data-wilayah.js',
+  './data-kader.js',
+  './data-pertanyaan.js'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-  self.skipWaiting(); // Memaksa versi baru langsung aktif
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  self.skipWaiting();
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-        return response || fetch(event.request);
-    })
-  );
+  event.respondWith(caches.match(event.request).then(response => { return response || fetch(event.request); }));
 });
 
-// Membersihkan cache lama agar memori HP kader tidak penuh
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
+  event.waitUntil(caches.keys().then(keys => {
+      return Promise.all(keys.map(key => { if (key !== CACHE_NAME) return caches.delete(key); }));
+  }));
 });
-
