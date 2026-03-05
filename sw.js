1bc0c0ccc1ef8ed6837442c63d70741a67d87c3d
tpkbuleleng <admin@ipekbbuleleng.com>
2026-03-05 10:30:59 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tpk-buleleng-v19';
+const CACHE_NAME = 'tpk-buleleng-v20';
 
 const urlsToCache = [
   './',
@@ -24,6 +24,7 @@ self.addEventListener('activate', event => {
       return Promise.all(keys.map(key => { if (key !== CACHE_NAME) return caches.delete(key); }));
   }));
 });
+
 
 
 
