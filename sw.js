edbf927d92ae2b232f666f0155a152354f945219
tpkbuleleng <admin@ipekbbuleleng.com>
2026-03-15 19:35:52 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tpk-buleleng-cache-v5';
+const CACHE_NAME = 'tpk-buleleng-cache-v6';
 
 // Daftar file pondasi (App Shell) yang HARUS disimpan secara offline
 const ASSETS_TO_CACHE = [
