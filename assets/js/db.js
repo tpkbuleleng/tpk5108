32d28ed92c47313ea7ab6fe66d85e518fdc3ce15
tpkbuleleng <admin@ipekbbuleleng.com>
2026-03-21 15:12:43 +0800

    Update db.js

--- assets/js/db.js
+++ assets/js/db.js
@@ -1,5 +1,5 @@
 const DB_NAME = 'TPK_Buleleng_DB';
-const DB_VERSION = 5; // 🔥 Naikkan ke Versi 5 untuk menyuntikkan tabel MASTER_MENU
+const DB_VERSION = 6; // 🔥 Naikkan ke Versi 6 untuk menyuntikkan MASTER_WIDGET
 
 export const initDB = () => {
     return new Promise((resolve, reject) => {
@@ -8,7 +8,7 @@ export const initDB = () => {
         request.onupgradeneeded = (e) => {
             const db = e.target.result;
             
-            // 🔥 HANCURKAN TABEL YANG BOCOR (PEMBERSIHAN PERMANEN)
+            // 🔥 HANCURKAN TABEL YANG BOCOR
             if (db.objectStoreNames.contains('master_user')) db.deleteObjectStore('master_user');
             if (db.objectStoreNames.contains('master_admin')) db.deleteObjectStore('master_admin');
 
@@ -16,7 +16,7 @@ export const initDB = () => {
             const stores = [
                 'master_kader', 'master_tim', 'master_tim_wilayah',
                 'master_pertanyaan', 'master_wilayah_bali', 'standar_antropometri', 
-                'master_kembang', 'master_wilayah', 'master_menu' // 🚀 INJEKSI TABEL MENU DISINI
+                'master_kembang', 'master_wilayah', 'master_menu', 'master_widget' // 🚀 INJEKSI TABEL WIDGET
             ];
             stores.forEach(store => { if (!db.objectStoreNames.contains(store)) db.createObjectStore(store, { autoIncrement: true }); });
             if (!db.objectStoreNames.contains('kader_session')) db.createObjectStore('kader_session', { keyPath: 'id' });
