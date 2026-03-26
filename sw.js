cdad4391a2b6815f5f4d332d7ce507b21846b931
tpkbuleleng <admin@ipekbbuleleng.com>
2026-03-26 20:35:09 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tpk-buleleng-v50'; // Kode versi, ubah angka jika ada update besar
+const CACHE_NAME = 'tpk-buleleng-vfinal01'; // Kode versi, ubah angka jika ada update besar
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
