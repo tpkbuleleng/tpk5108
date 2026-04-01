1cf2fd8fd5668cd22a53c292d4b38bea43b37e26
tpkbuleleng <admin@ipekbbuleleng.com>
2026-04-01 11:10:25 +0800

    Update session.js

--- assets/js/session.js
+++ assets/js/session.js
@@ -1,1 +1,25 @@
+window.Session = {
+  getToken() {
+    return StorageHelper.get(APP_CONFIG.STORAGE_KEYS.SESSION_TOKEN, '');
+  },
 
+  setToken(token) {
+    return StorageHelper.set(APP_CONFIG.STORAGE_KEYS.SESSION_TOKEN, token || '');
+  },
+
+  getProfile() {
+    return StorageHelper.get(APP_CONFIG.STORAGE_KEYS.PROFILE, null);
+  },
+
+  setProfile(profile) {
+    return StorageHelper.set(APP_CONFIG.STORAGE_KEYS.PROFILE, profile || null);
+  },
+
+  isLoggedIn() {
+    return !!this.getToken() && !!this.getProfile();
+  },
+
+  logout() {
+    StorageHelper.clearSession();
+  }
+};
