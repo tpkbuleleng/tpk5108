434a2ac9f73065b42abf39cf874cbe42385d23e7
tpkbuleleng <admin@ipekbbuleleng.com>
2026-04-01 11:24:07 +0800

    Update sasaran-service.js

--- assets/js/modules/kader/sasaran-service.js
+++ assets/js/modules/kader/sasaran-service.js
@@ -1,1 +1,19 @@
+window.SasaranService = {
+  async getTimWilayah() {
+    return Api.post('getTimWilayah', {});
+  },
 
+  async getSasaranByTim(filters = {}) {
+    return Api.post('getSasaranByTim', {
+      keyword: filters.keyword || '',
+      jenis_sasaran: filters.jenis_sasaran || '',
+      status_sasaran: filters.status_sasaran || ''
+    });
+  },
+
+  async getSasaranDetail(idSasaran) {
+    return Api.post('getSasaranDetail', {
+      id_sasaran: idSasaran
+    });
+  }
+};
