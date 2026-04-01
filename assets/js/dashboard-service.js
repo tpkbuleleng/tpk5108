d8f8275ae3ff8b3422b6b0f8ead58f513fedc4b7
tpkbuleleng <admin@ipekbbuleleng.com>
2026-04-01 20:53:33 +0800

    Update dashboard-service.js

--- assets/js/dashboard-service.js
+++ assets/js/dashboard-service.js
@@ -1,5 +1,15 @@
 window.DashboardService = {
   async getDashboardKaderSummary() {
     return Api.post('getDashboardKaderSummary', {});
+  },
+
+  async getRekapKader(bulan) {
+    return Api.post('getRekapKader', {
+      bulan: bulan || ''
+    });
+  },
+
+  async getTimWilayah() {
+    return Api.post('getTimWilayah', {});
   }
 };
