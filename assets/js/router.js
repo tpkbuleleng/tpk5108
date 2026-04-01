4b1c323eda385bf08c8760b84e074a26597fdc8f
tpkbuleleng <admin@ipekbbuleleng.com>
2026-04-01 11:13:20 +0800

    Update router.js

--- assets/js/router.js
+++ assets/js/router.js
@@ -1,1 +1,13 @@
+window.Router = {
+  toLogin() {
+    UI.showScreen('login-screen');
+  },
 
+  toDashboard() {
+    UI.showScreen('dashboard-screen');
+  },
+
+  toSplash() {
+    UI.showScreen('splash-screen');
+  }
+};
