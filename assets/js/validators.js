32329dee0c0056ca2781d22bd08f9dcf2a5f47be
tpkbuleleng <admin@ipekbbuleleng.com>
2026-04-01 11:11:16 +0800

    Update validators.js

--- assets/js/validators.js
+++ assets/js/validators.js
@@ -1,1 +1,13 @@
+window.Validators = {
+  digitsOnly(value = '') {
+    return String(value).replace(/\D/g, '');
+  },
 
+  isPassword16(value = '') {
+    return /^\d{16}$/.test(String(value));
+  },
+
+  isRequired(value) {
+    return value !== null && value !== undefined && String(value).trim() !== '';
+  }
+};
