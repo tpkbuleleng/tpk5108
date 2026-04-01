b812af710cd490fa4fb677dc5f97ee4477756718
tpkbuleleng <admin@ipekbbuleleng.com>
2026-04-01 22:24:02 +0800

    Update app-config.js

--- assets/js/app-config.js
+++ assets/js/app-config.js
@@ -1,16 +1,46 @@
 window.APP_CONFIG = {
   APP_NAME: 'TPK KABUPATEN BULELENG',
+
   APP_VERSION: '2.1.0',
+
   API_URL: '',
+
   REQUEST_TIMEOUT_MS: 30000,
+
   DEBUG: true,
+
   STORAGE_KEYS: {
     API_URL: 'apiUrl',
     SESSION_TOKEN: 'sessionToken',
     PROFILE: 'profile',
     BOOTSTRAP: 'bootstrapRef',
+
     SELECTED_SASARAN: 'selectedSasaran',
+
     SYNC_QUEUE: 'syncQueue',
-    LAST_SYNC_AT: 'lastSyncAt'
-  }
+    LAST_SYNC_AT: 'lastSyncAt',
+
+    REGISTRASI_DRAFT: 'draftRegistrasi',
+    PENDAMPINGAN_DRAFT: 'draftPendampingan',
+
+    DEVICE_ID: 'stable_device_id',
+
+    EDIT_PENDAMPINGAN: 'editPendampingan',
+
+    DASHBOARD_CACHE: 'dashboardCache'
+  },
+
+  STATUS_SASARAN: [
+    'AKTIF',
+    'PERLU_REVIEW',
+    'NONAKTIF',
+    'SELESAI'
+  ],
+
+  STATUS_KUNJUNGAN: [
+    'BERHASIL',
+    'TIDAK_BERHASIL',
+    'DITUNDA',
+    'FOLLOW_UP'
+  ]
 };
