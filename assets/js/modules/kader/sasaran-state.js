aac32bd32ef53be650ea004077d9dd6d00f651ec
tpkbuleleng <admin@ipekbbuleleng.com>
2026-04-01 11:23:46 +0800

    Update sasaran-state.js

--- assets/js/modules/kader/sasaran-state.js
+++ assets/js/modules/kader/sasaran-state.js
@@ -1,1 +1,21 @@
+window.SasaranState = {
+  getList() {
+    return StorageHelper.get('sasaranListCache', []);
+  },
 
+  setList(items) {
+    return StorageHelper.set('sasaranListCache', Array.isArray(items) ? items : []);
+  },
+
+  getSelected() {
+    return StorageHelper.get(APP_CONFIG.STORAGE_KEYS.SELECTED_SASARAN, null);
+  },
+
+  setSelected(item) {
+    return StorageHelper.set(APP_CONFIG.STORAGE_KEYS.SELECTED_SASARAN, item || null);
+  },
+
+  clearSelected() {
+    StorageHelper.remove(APP_CONFIG.STORAGE_KEYS.SELECTED_SASARAN);
+  }
+};
