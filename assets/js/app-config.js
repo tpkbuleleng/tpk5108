97dc5bfb510751e8f21c10258a977feefefa1dc9
tpkbuleleng <admin@ipekbbuleleng.com>
2026-04-03 11:21:35 +0800

    Update app-config.js

--- assets/js/app-config.js
+++ assets/js/app-config.js
@@ -1,46 +1,17 @@
 window.APP_CONFIG = {
   APP_NAME: 'TPK KABUPATEN BULELENG',
-
   APP_VERSION: '2.1.0',
 
-  API_URL: '',
+  API_BASE_URL: 'https://script.google.com/macros/s/ISI_URL_WEBAPP_ANDA/exec',
 
-  REQUEST_TIMEOUT_MS: 30000,
+  SHOW_BACKEND_SETTINGS: false,
 
-  DEBUG: true,
+  LOGIN_LOGO_URL: 'assets/img/logo-tpk.svg',
 
   STORAGE_KEYS: {
-    API_URL: 'apiUrl',
-    SESSION_TOKEN: 'sessionToken',
-    PROFILE: 'profile',
-    BOOTSTRAP: 'bootstrapRef',
-
-    SELECTED_SASARAN: 'selectedSasaran',
-
-    SYNC_QUEUE: 'syncQueue',
-    LAST_SYNC_AT: 'lastSyncAt',
-
-    REGISTRASI_DRAFT: 'draftRegistrasi',
-    PENDAMPINGAN_DRAFT: 'draftPendampingan',
-
-    DEVICE_ID: 'stable_device_id',
-
-    EDIT_PENDAMPINGAN: 'editPendampingan',
-
-    DASHBOARD_CACHE: 'dashboardCache'
-  },
-
-  STATUS_SASARAN: [
-    'AKTIF',
-    'PERLU_REVIEW',
-    'NONAKTIF',
-    'SELESAI'
-  ],
-
-  STATUS_KUNJUNGAN: [
-    'BERHASIL',
-    'TIDAK_BERHASIL',
-    'DITUNDA',
-    'FOLLOW_UP'
-  ]
+    API_URL: 'tpk_api_url',
+    SESSION_TOKEN: 'tpk_session_token',
+    PROFILE: 'tpk_profile',
+    DEVICE_ID: 'tpk_device_id'
+  }
 };
