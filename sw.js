f7478388d233cb6fa15dce642b1545cf01146e34
tpkbuleleng <admin@ipekbbuleleng.com>
2026-04-19 19:51:53 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,94 +1,107 @@
-const APP_SHELL_CACHE = 'tpk-shell-v2-1-2';
-const STATIC_CACHE = 'tpk-static-v2-1-2';
-const APP_SHELL_ASSETS = [
+
+const SW_VERSION = 'tpk-sw-stage1-v1';
+const SHELL_CACHE = 'tpk-shell-' + SW_VERSION;
+const ASSET_CACHE = 'tpk-assets-' + SW_VERSION;
+const APP_SHELL = [
   './',
   './index.html',
   './app.css',
   './manifest.webmanifest',
+  './assets/img/logo.png',
+  './assets/img/logo-192.png',
   './js/config.js',
-  './js/utils.js',
   './js/storage.js',
   './js/state.js',
   './js/api.js',
   './js/auth.js',
-  './js/router.js',
   './js/bootstrap.js',
   './js/ui.js',
-  './js/app.js',
-  './assets/img/logo.png',
-  './assets/img/logo-192.png'
+  './js/app.js'
 ];
 
-self.addEventListener('install', function(event) {
+function isPrivateRequest(requestUrl, method) {
+  const href = String(requestUrl || '');
+  const upperMethod = String(method || 'GET').toUpperCase();
+
+  if (upperMethod !== 'GET') return true;
+  if (/script\.google\.com/i.test(href)) return true;
+  if (/script\.googleusercontent\.com/i.test(href)) return true;
+  if (/\/exec(\?|$)/i.test(href)) return true;
+
+  return false;
+}
+
+function isSameOriginAsset(url) {
+  return url.origin === self.location.origin &&
+    /\.(?:css|js|png|jpg|jpeg|svg|webp|gif|ico|woff2?|ttf|eot|json|webmanifest)$/i.test(url.pathname);
+}
+
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(APP_SHELL_CACHE)
-      .then(function(cache) {
-        return cache.addAll(APP_SHELL_ASSETS);
-      })
-      .then(function() {
-        return self.skipWaiting();
-      })
+    caches.open(SHELL_CACHE).then((cache) => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())
   );
 });
 
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(keys) {
-      return Promise.all(
-        keys
-          .filter(function(key) {
-            return key !== APP_SHELL_CACHE && key !== STATIC_CACHE;
-          })
-          .map(function(key) {
-            return caches.delete(key);
-          })
-      );
-    }).then(function() {
-      return self.clients.claim();
-    })
-  );
+self.addEventListener('activate', (event) => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map((key) => {
+      if (key !== SHELL_CACHE && key !== ASSET_CACHE) {
+        return caches.delete(key);
+      }
+      return Promise.resolve();
+    }));
+
+    if ('navigationPreload' in self.registration) {
+      try {
+        await self.registration.navigationPreload.enable();
+      } catch (err) {}
+    }
+
+    await self.clients.claim();
+  })());
 });
 
-self.addEventListener('fetch', function(event) {
-  var request = event.request;
-  if (request.method !== 'GET') return;
+self.addEventListener('fetch', (event) => {
+  const request = event.request;
+  const url = new URL(request.url);
 
-  var url = new URL(request.url);
-
-  if (url.origin !== self.location.origin) {
-    return;
-  }
-
-  if (url.pathname.indexOf('/exec') !== -1 || url.hostname.indexOf('script.google') !== -1 || url.hostname.indexOf('googleusercontent.com') !== -1) {
+  if (isPrivateRequest(url.href, request.method)) {
     return;
   }
 
   if (request.mode === 'navigate') {
-    event.respondWith(
-      fetch(request).catch(function() {
+    event.respondWith((async () => {
+      try {
+        const preload = await event.preloadResponse;
+        if (preload) return preload;
+
+        const network = await fetch(request);
+        const cache = await caches.open(SHELL_CACHE);
+        cache.put(request, network.clone());
+        return network;
+      } catch (err) {
+        const cached = await caches.match(request);
+        if (cached) return cached;
         return caches.match('./index.html');
-      })
-    );
+      }
+    })());
     return;
   }
 
-  if (/\.(png|jpg|jpeg|webp|svg|gif|css|js|html|webmanifest)$/i.test(url.pathname)) {
-    event.respondWith(
-      caches.match(request).then(function(cached) {
-        var networkFetch = fetch(request).then(function(response) {
-          if (response && response.ok) {
-            var clone = response.clone();
-            caches.open(STATIC_CACHE).then(function(cache) {
-              cache.put(request, clone);
-            });
-          }
-          return response;
-        }).catch(function() {
-          return cached;
-        });
+  if (isSameOriginAsset(url)) {
+    event.respondWith((async () => {
+      const cache = await caches.open(ASSET_CACHE);
+      const cached = await cache.match(request);
 
-        return cached || networkFetch;
-      })
-    );
+      const networkPromise = fetch(request).then((response) => {
+        if (response && response.ok) {
+          cache.put(request, response.clone());
+        }
+        return response;
+      }).catch(() => null);
+
+      return cached || networkPromise || fetch(request);
+    })());
   }
 });
