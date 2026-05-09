50fb4c7940c6d2352da97c7080d9f2409444611c
tpkbuleleng <admin@ipekbbuleleng.com>
2026-05-09 23:25:16 +0800

    Update router.js

--- js/router.js
+++ js/router.js
@@ -37,7 +37,7 @@
       { src: './js/views/rekapKaderView.js', globalName: 'RekapKaderView' }
     ],
     superAdmin: [
-      { src: './js/views/superAdminDashboardView.js?v=20260509-5E-R4D-A8', globalName: 'SuperAdminDashboardView' }
+      { src: './js/views/superAdminDashboardView.js?v=20260509-5E-R4D-A8-R1', globalName: 'SuperAdminDashboardView' }
     ]
   };
 
