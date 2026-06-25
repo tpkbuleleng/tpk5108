12d5e3af47ccb992a784f3776a60144a09c17f91
tpkbuleleng <admin@ipekbbuleleng.com>
2026-06-25 15:00:57 +0800

    Paket HARGANAS-4-R1

--- js/views/harganasView.js
+++ js/views/harganasView.js
@@ -1,7 +1,7 @@
 (function (window, document) {
   'use strict';
 
-  var VIEW_VERSION = 'HARGANAS-4-VIEW-20260625';
+  var VIEW_VERSION = 'HARGANAS-4-R1-VIEW-20260625';
   var bound = false;
 
   function byId(id) { return document.getElementById(id); }
