d8de1e6d3b6340ef646284a6487ce10a3f3f8767
tpkbuleleng <admin@ipekbbuleleng.com>
2026-06-25 14:22:44 +0800

    Paket HARGANAS-3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SW_VERSION = 'tpk-sw-harganas-2d-20260625-01';
+const SW_VERSION = 'tpk-sw-harganas-3-20260625-01';
 const SHELL_CACHE = 'tpk-shell-' + SW_VERSION;
 const ASSET_CACHE = 'tpk-assets-' + SW_VERSION;
 
@@ -21,11 +21,13 @@ const APP_SHELL = [
   './js/api.js',
   './js/auth.js',
   './js/app.js',
+  './js/services/harganasWatermarkService.js',
   './js/views/appLandingView.js',
   './js/services/harganasValidationService.js',
   './js/services/harganasDraftService.js',
   './js/services/harganasGpsService.js',
   './js/services/harganasMediaService.js',
+  './js/services/harganasWatermarkService.js',
   './js/services/harganasVideoService.js',
   './js/views/harganasView.js'
 ];
