c424d25104e4ca27ed0a4f9a3a43818df96a9dd3
tpkbuleleng <admin@ipekbbuleleng.com>
2026-06-25 16:25:20 +0800

    Paket HARGANAS-4A

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SW_VERSION = 'tpk-sw-harganas-4-r1-20260625-01';
+const SW_VERSION = 'tpk-sw-harganas-4a-20260625-01';
 const SHELL_CACHE = 'tpk-shell-' + SW_VERSION;
 const ASSET_CACHE = 'tpk-assets-' + SW_VERSION;
 
